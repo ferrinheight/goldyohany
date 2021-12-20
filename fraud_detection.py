be8eefdf02edd17061df677905928ac4843a2b37
ferrinheight <91694029+ferrinheight@users.noreply.github.com>
2021-12-19 22:50:20 -0800

    Update fraud_detection.py

--- fraud_detection.py
+++ fraud_detection.py
@@ -13,4 +13,15 @@ print("Number of Genuine transactions: ", non_fraud)
 print("Number of Fraud transactions: ", fraud)
 print("Percentage of Fraud transactions: {:.4f}".format(fraud_percent))
 
-sns.relplot(x = 'Amount', y ='Time', hue ='Class', data=df)
+# seaborn not working for christ0ferrin
+##sns.relplot(x = 'Amount', y ='Time', hue ='Class', data=df)
+
+
+
+labels = ["Genuine", "Fraud"]
+count_classes = df.value_counts(df['Class'], sort= True)
+count_classes.plot(kind = "bar", rot = 0)
+plt.title("Visualization of Labels")
+plt.ylabel("Count")
+plt.xticks(range(2), labels)
+plt.show()
